970becf86746614111d243bf6d6b4ba30ec9b660
Darklight437 <darklight437@gmail.com>
2019-01-25 21:22:17 +1100

    Game timer static class for timer and time multiplier

---
+++ Catte/Assets/GameTimer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class GameTimer
+{
+    public static float timer;
+    public static float timeMultiplier;
+
+}
