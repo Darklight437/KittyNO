d90b0911a382840783460fe5484740f7119a2974
Darklight437 <darklight437@gmail.com>
2019-01-25 22:16:20 +1100

    Added some empty scripts to plan out what will be needed
    filled in intial stuff in the paw script

---
+++ Catte/Assets/Scripts/Paw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paw : MonoBehaviour {
+    [SerializeField]
+    private Transform TimeZeroPos;
+    [SerializeField]
+    private Transform Time100Pos;
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
