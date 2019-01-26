8bbd82714f8b67052c7cdd8d0f045f760db397f4
Darklight437 <darklight437@gmail.com>
2019-01-26 15:55:53 +1100

    yarn distraction added
    animation used for motion

---
+++ Catte/Assets/Scripts/Distractions/Yarn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yarn : Distraction
+{
+
+
+	// Use this for initialization
+	new protected void Start ()
+    {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
