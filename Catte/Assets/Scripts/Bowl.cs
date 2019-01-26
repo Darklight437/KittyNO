757d537135f6c6b8c1a276f80367523413518ba8
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2019-01-26 11:34:54 +1100

    paw pushes bowl now

---
+++ Catte/Assets/Scripts/Bowl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowl : MonoBehaviour
+{
+    private Transform paw;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        // find the paw
+        paw = GameObject.Find("Paw").transform;
+
+        offset = transform.position - paw.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position.x - paw.position.x > offset.x)
+        {
+            transform.position = paw.position + offset;
+        }
+    }
+}
