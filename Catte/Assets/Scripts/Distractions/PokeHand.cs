9e74d267aee281ef57a31730cdd7a0fe13621fe9
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2019-01-26 15:19:22 +1100

    added poke

---
+++ Catte/Assets/Scripts/Distractions/PokeHand.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokeHand : Distraction
+{
+    [SerializeField]
+    private float pokeSpeed;
+
+    [SerializeField]
+    private float pokeDistance;
+
+    private Vector3 center;
+    private float createTime;
+
+    new protected void Start()
+    {
+        base.Start();
+
+        createTime = Time.time;
+        center = transform.position;
+    }
+
+    private void Update()
+    {
+        float t = (Time.time - createTime) * pokeSpeed;
+        transform.position = center + transform.right * Mathf.Sin(t) * pokeDistance;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(transform.position - transform.right * pokeDistance, transform.position + transform.right * pokeDistance);
+    }
+}
--- Catte/Assets/Scripts/UIController.cs
+++ Catte/Assets/Scripts/UIController.cs
@@ -7,8 +7,16 @@ public class UIController : MonoBehaviour
     [SerializeField]
     private GameObject petHand;
 
+    [SerializeField]
+    private GameObject pokeHand;
+
     public void spawnPetHand()
     {
         Instantiate(petHand);
+    }
+
+    public void spawnPokeHand()
+    {
+        Instantiate(pokeHand);
     }
 }
