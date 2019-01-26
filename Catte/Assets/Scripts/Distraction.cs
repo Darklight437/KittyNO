895354c2aa527644dc40526378fcefe07b6754ad
Darklight437 <darklight437@gmail.com>
2019-01-26 18:15:31 +1100

    Aura, eye shake and final sequence script

--- Catte/Assets/Scripts/Distraction.cs
+++ Catte/Assets/Scripts/Distraction.cs
@@ -25,22 +25,4 @@ public class Distraction : MonoBehaviour
     }
 
 
-    //no function should be nessecary for playing the animation as we can just play on awake
-
-
-    //LIST OF DISTRACTION OBJECTS AS CURRENT
-    //8 TOTAL
-    //hand poke
-    //hand pet
-    //laser pointer
-    //string
-    //catnip
-    //stick + bird (the bouncy toy on a string)
-    //a cheezeburger OR loops (not sure which yet but obvious meme)
-    //the spray bottle(simple particles)
-    //
-    //
-    //each will have just one animation and only exist for a few seconds
-
-
 }
---
+++ Catte/Assets/Scripts/FinalSequence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalSequence : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Aura;
+    [SerializeField]
+    private GameObject EyeL;
+    [SerializeField]
+    private GameObject EyeR;
+
+    private Vector3 eyeRPosition;
+    private Vector3 eyeLPosition;
+
+    public bool FinalEvent = false;
+    private bool i = true;
+
+    private void Start()
+    {
+        eyeRPosition = EyeR.transform.position;
+        eyeLPosition = EyeL.transform.position;
+    }
+
+    void Update ()
+    {
+        if (FinalEvent && i)
+        {
+            EyeL.transform.position = eyeLPosition + (Vector3)Random.insideUnitCircle * 0.2f;
+            EyeR.transform.position = eyeRPosition + (Vector3)Random.insideUnitCircle * 0.2f;
+            Aura.SetActive(true);
+            
+        }
+         i = !i;
+	}
+}
