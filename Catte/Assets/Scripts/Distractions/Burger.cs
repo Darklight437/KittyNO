470b50d117439604756afd573ec58f7b1a108719
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2019-01-27 11:15:44 +1100

    finished all the distractions

--- Catte/Assets/Scripts/Distractions/Burger.cs
+++ Catte/Assets/Scripts/Distractions/Burger.cs
@@ -5,17 +5,63 @@ using UnityEngine;
 public class Burger : Distraction
 {
     [SerializeField]
+    private float spawnHeight;
 
-	new protected void Start ()
+    [SerializeField]
+    private float tableHeight;
+
+    [SerializeField]
+    private float width;
+
+    [SerializeField]
+    private float fallSpeed;
+
+    [SerializeField]
+    private GameObject loop;
+
+    [SerializeField]
+    private float loopRadius;
+
+    [SerializeField]
+    private Color[] loopColors;
+
+    new protected void Start ()
     {
         base.Start();
-	}
+
+        addLoops();
+
+        // move to a random point above the screen
+        transform.position = new Vector3(Random.Range(0, width) - width * 0.5f, spawnHeight, transform.position.z);
+    }
+
+    private void Update()
+    {
+        // fall until it hits the table
+        if (transform.position.y > tableHeight)
+        {
+            transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+        }
+    }
 
     void addLoops()
     {
         for(int i = 0; i < 15; i++)
         {
-
+            GameObject currentLoop = Instantiate(loop);
+            currentLoop.transform.position = transform.position + (Vector3)(Random.insideUnitCircle) * loopRadius;
+            currentLoop.transform.parent = transform;
+            currentLoop.GetComponent<Renderer>().material.color = loopColors[Random.Range(0, loopColors.Length)];
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(Vector3.up * spawnHeight + Vector3.left * width * 0.5f, Vector3.up * spawnHeight + Vector3.right * width * 0.5f);
+        Gizmos.DrawLine(Vector3.up * tableHeight + Vector3.left * width * 0.5f, Vector3.up * tableHeight + Vector3.right * width * 0.5f);
+
+        Gizmos.DrawWireSphere(transform.position, loopRadius);
     }
 }
